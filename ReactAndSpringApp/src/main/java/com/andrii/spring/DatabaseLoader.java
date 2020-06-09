18220d044d190eecb0e603a62fc9c3ba8b44d3a0
Andrii-25 <66208186+Andrii-25@users.noreply.github.com>
2020-06-09 17:40:31 +0300

    fixed react

---
+++ ReactAndSpringApp/src/main/java/com/andrii/spring/DatabaseLoader.java
@@ -0,0 +1,21 @@
+package com.andrii.spring;
+
+import org.springframework.stereotype.Component;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+	private final EmployeeRepository repository;
+
+	@Autowired 
+	public DatabaseLoader(EmployeeRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+	}
+}
